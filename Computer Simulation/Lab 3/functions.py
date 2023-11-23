2978c0c8fdb96c864f55ac942b8066d80e479d49
Jacob <jakubwiesniak102@gmail.com>
2023-11-23 23:48:49 +0100

    Setting up next exercise

---
+++ Computer Simulation/Lab 3/functions.py
@@ -0,0 +1,96 @@
+import random
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def determine_score(distance):
+    if distance < 0.1:
+        return 6
+    elif distance < 0.2:
+        return 5
+    elif distance < 0.3:
+        return 4
+    elif distance < 0.4:
+        return 3
+    elif distance < 0.5:
+        return 2
+    elif distance < 0.6:
+        return 1
+    else:
+        return 0
+
+
+def generate_shield(coordinates, x=[], y=[], x_1=[], y_1=[],  rays=None, type="single_a"):
+    if rays is None:
+        rays = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+    fig, ax = plt.subplots()
+    for radius in rays:
+        circle = plt.Circle(coordinates, radius, edgecolor='b', facecolor='none')
+        ax.add_patch(circle)
+
+    ax.set_aspect('equal', adjustable='box')
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    if type == "single_a":
+        plt.scatter(x, y, color='red', label='Points')
+        plt.title("Shoot A:")
+    if type == "single_b":
+        plt.scatter(x, y, color='blue', label='Points')
+        plt.title("Shoot B:")
+    if type == "multiple":
+        plt.scatter(x, y, color='red', label='Points A')
+        plt.scatter(x_1, y_1, color='blue', label='Points B')
+
+    plt.xlabel("axis X")
+    plt.ylabel("axis Y")
+    plt.grid(True)
+    plt.show()
+
+
+def shoot_a():
+    d_x = np.random.normal(0,1)
+    x = random.uniform(-0.75, 0.75)
+    y = random.uniform(-0.75, 0.75)
+    r = math.sqrt(pow(x,2) + pow(y,2) )
+    return [x+d_x, y, r]
+
+
+def shoot_b():
+    d_x = np.random.normal(0, 1)
+    r = random.uniform(0, 0.75)
+    phi = random.uniform(-3.14, 3.14)
+    x = r*math.cos(phi)
+    y = r*math.sin(phi)
+    return [x+d_x, y, r]
+
+
+def simulation(type_of_shoot):
+    temp = 0
+    x_points = []
+    y_points = []
+    for _ in range(10):
+        if type_of_shoot == "A":
+            x, y, r = shoot_a()
+            temp += (determine_score(r))
+            x_points.append(x)
+            y_points.append(y)
+        elif type_of_shoot == "B":
+            x, y, r = shoot_b()
+            temp += (determine_score(r))
+            x_points.append(x)
+            y_points.append(y)
+    return [temp/10, x_points, y_points]
+
+
+def generate_histogram(avg_a, avg_b):
+    fig, axs = plt.subplots(1, 2)
+
+    axs[0].hist(avg_a, bins=15, color='blue', alpha=0.7)
+    axs[0].set_title("Shooting Range A")
+
+    axs[1].hist(avg_b, bins=15, color='red', alpha=0.7)
+    axs[1].set_title("Shooting Range B")
+
+    plt.tight_layout()
+    plt.show()
---
+++ Computer Simulation/Lab 3/main.py
@@ -0,0 +1,74 @@
+import functions as f
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+"""
+Configuration file for a proper setting up a simulation
+"""
+
+# setting number of simulations
+number_of_simulations = 31
+# confidence level
+confidence_level = 0.05
+
+
+# shoot A
+avg_a = []
+x_a = []
+y_a = []
+for a in range(number_of_simulations):
+    score, x_temp, y_temp = f.simulation(type_of_shoot='A')
+    avg_a.append(score)
+    x_a += x_temp
+    y_a += y_temp
+f.generate_shield((0,0), x=x_a, y=y_a, type="single_a")
+
+# shoot B
+avg_b = []
+x_b = []
+y_b = []
+for b in range(number_of_simulations):
+    score, x_temp, y_temp = f.simulation(type_of_shoot='B')
+    avg_b.append(score)
+    x_b += x_temp
+    y_b += y_temp
+f.generate_shield((0,0), x=x_b, y=y_b, type="single_b")
+
+# Plot with combined Shot A and B
+f.generate_shield((0,0), x=x_a, y=y_a, x_1=x_b, y_1=y_b, type="multiple")
+
+# Creating Plots to compare both shootings methods
+f.generate_histogram(avg_a, avg_b)
+
+print(f"Average for shooting range a { sum(avg_a)/ len(avg_a) }")
+print(f"Average for shooting range b { sum(avg_b)/ len(avg_b) }")
+
+# Statistical Test for A
+# Testing our hypothesis avg_a is from normal distribution
+# Ho avg_a is from normal distribution
+# Ha avg_a isn't from normal distribution
+
+t_stat, p_value = stats.shapiro(avg_a)
+print(f"Statystyka t: {t_stat}")
+print(f"P-wartość: {p_value}")
+
+if p_value > confidence_level:
+    print("Nie mamy wystarczających dowodów aby odrzucić H0")
+else:
+    print("Mamy wystarczający dowód aby odrzucić H0 przyjmujemy hipotezę alternatywną Ha czyli avg_a != avg_b")
+
+
+# Statistical Test for B
+# Testing our hypothesis avg_b is from normal distribution
+# Ho avg_b is from normal distribution
+# Ha avg_b isn't from normal distribution
+
+t_stat, p_value = stats.shapiro(avg_b)
+print(f"Statystyka t: {t_stat}")
+print(f"P-wartość: {p_value}")
+
+if p_value > confidence_level:
+    print("Nie mamy wystarczających dowodów aby odrzucić H0")
+else:
+    print("Mamy wystarczający dowód aby odrzucić H0 przyjmujemy hipotezę alternatywną Ha czyli avg_a != avg_b")
+
